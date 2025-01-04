12492a3c99c21ee7c994bb468b3e7873826bd355
pixelvikas <tech.pixelvikas@gmail.com>
2025-01-04 17:07:58 +0530

    Pages | Components | Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Aboutus from "./pages/Aboutus";
+import Services from "./pages/Services"
+import Projects from "./pages/Projects"
+import Blogs from "./pages/Blog"
+import Contactus from "./pages/Contactus"
+import SingleBlog from "./pages/SingleBlog"
+import SingleProject from "./pages/SingleProject"
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<Aboutus />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact-us" element={<Contactus />} />
+        <Route path="/projects/:id" element={<SingleProject />} />
+        <Route path="/blogs/:id" element={<SingleBlog />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
