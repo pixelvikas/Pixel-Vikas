8a2e164569502a7483f385f8942cdbb118529d79
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-01-10 00:26:04 +0530

    ABOUT US - Hero | About us | Vision | Mission

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import "./Aboutus.css";
 
 const Aboutus = () => {
   return (
-    <div>Aboutus</div>
-  )
-}
+    <>
+      <div className="hero-section">
+        <h1 className="hero-title">About Us</h1>
+      </div>
+      <div className="projects">
+        <div className="projects-header">
+          <div className="projects-title">
+            <h1 className="highlight-text">*WHO ARE WE!?</h1>
+            <h1>
+              ABOUT <span className="span-yellow">US </span>
+            </h1>
+          </div>
+          <p className="projects-description">
+            We transform innovative ideas into captivating and memorable digital
+            experiences that engage and inspire
+          </p>
+        </div>
 
-export default Aboutus
+        <div className="about-us-content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+
+      <div className="services">
+        <div className="services-header">
+          <div className="services-title">
+            <h1 className="highlight-text">*WHAT DO WE AIM?!</h1>
+            <h1>
+              OUR <span className="span-yellow">VISION</span>
+            </h1>
+          </div>
+        </div>
+        <div className="vision-content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+
+      <div className="projects">
+        <div className="projects-header">
+          <div className="projects-title">
+            <h1 className="highlight-text">*WHAT ARE OUR AMBITIONS!?</h1>
+            <h1>
+              OUR <span className="span-yellow">MISSION</span>
+            </h1>
+          </div>
+        </div>
+
+        <div className="about-us-content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Aboutus;
