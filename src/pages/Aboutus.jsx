fff353dffa2d12091e796a01e648c01a9db96f85
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-01-11 12:05:32 +0530

    About us - Client cestion Completed

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -3,6 +3,18 @@ import PV from "/assets/PV.svg";
 import { FiArrowUpRight } from "react-icons/fi";
 import "./Aboutus.css";
 
+import client1 from "/assets/client1.png";
+import client3 from "/assets/client3.png";
+import client4 from "/assets/client4.png";
+import client5 from "/assets/client5.png";
+import client6 from "/assets/client6.png";
+import client7 from "/assets/client7.png";
+import client9 from "/assets/client9.png";
+import client10 from "/assets/client10.png";
+import client11 from "/assets/client11.png";
+import client12 from "/assets/client12.png";
+import client13 from "/assets/client13.png";
+
 const Aboutus = () => {
   const [data, setData] = useState([]);
 
@@ -13,6 +25,7 @@ const Aboutus = () => {
       .then((json) => setData(json))
       .catch((error) => console.error("Error loading data:", error));
   }, []);
+
   return (
     <>
       <div className="section-hero">
@@ -123,6 +136,34 @@ const Aboutus = () => {
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="ourclients">
+        <div className="ourclients-header">
+          <div className="ourclients-title">
+            <h1 className="highlight-text">
+              *CLIENT’S ARE STILL CONNECTED WITH US!
+            </h1>
+            <h1>
+              EXPLORE <br />
+              <span className="span-white"> OUR </span> CLIENT’S
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="client">
+        <img src={client1} alt="Client 1" />
+        <img src={client3} alt="Client 3" />
+        <img src={client4} alt="Client 4" />
+        <img src={client5} alt="Client 5" />
+        <img src={client6} alt="Client 6" />
+        <img src={client7} alt="Client 7" />
+        <img src={client9} alt="Client 9" />
+        <img src={client10} alt="Client 10" />
+        <img src={client11} alt="Client 11" />
+        <img src={client12} alt="Client 12" />
+        <img src={client13} alt="Client 13" />
       </div>
     </>
   );
