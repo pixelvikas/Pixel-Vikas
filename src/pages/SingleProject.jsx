7561fc2e9d1fcb71934c3c6d38b216ac34dcb895
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-01-14 00:32:56 +0530

    SingleProject - Project Glimpse section completed

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -6,8 +6,34 @@ import p1b from "/assets/project1b.png";
 import p1c from "/assets/project1c.png";
 import p1d from "/assets/project1d.png";
 import "./SingleProject.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const SingleProject = () => {
+  // Array of image URLs
+  const images = [
+    "/assets/rajkosh1.png",
+    "/assets/rajkosh2.png",
+    "/assets/rajkosh3.png",
+    "/assets/rajkosh4.png",
+    "/assets/rajkosh5.png",
+    "/assets/rajkosh6.png",
+  ];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(true); // Trigger fade-out effect
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length); // Update the image index
+        setFade(false); // Reset fade for fade-in effect
+      }, 1000); // Sync fade-out with CSS transition duration
+    }, 5000); // Change image every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, [images.length]);
+
   return (
     <>
       <div className="singleproject-hero">
@@ -59,12 +85,27 @@ const SingleProject = () => {
         </div>
         <div className="project-folder">
           <div className="project-folder-content">
-            <img src={p1a} alt="Service 1" className="folder-image" />
+            <img src={p1c} alt="Service 1" className="folder-image" />
           </div>
           <div className="folder">
-            <img src={p1b} alt="Service 2" className="folder-image" />
+            <img src={p1d} alt="Service 2" className="folder-image" />
           </div>
         </div>
+
+        <div className="singleproject-detail-header">
+          <div className="singleproject-detail-title">
+            <h1>
+              PROJECT <br /> <span className="span-white">GLIMPSE </span>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="homepage" id="home">
+        <img
+          className={`homepage-img ${fade ? "fade-out" : "fade-in"}`}
+          src={images[currentImageIndex]}
+          alt={`Background ${currentImageIndex + 1}`}
+        />
       </div>
     </>
   );
