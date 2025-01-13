e8c25630813f1dc8623de7b755e9cb4f8824045d
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-01-13 15:51:22 +0530

    SingleProject - Hero section completed

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Mockup from "/assets/devicemockup.png";
+import "./SingleProject.css";
 
 const SingleProject = () => {
   return (
-    <div>SingleProject</div>
-  )
-}
+    <>
+      <div className="singleproject-hero">
+        <div className="singleproject-content">
+          <h1 className="singleproject-title">Rajkosh</h1>
+          <div className="singleproject-services">
+            <h1 className="singleproject-services-header">Services:</h1>
+            <h2 className="singleproject-details">
+              UX / UI | Social Media | Server
+            </h2>
+          </div>
+        </div>
+        <div className="singleproject-mockup">
+          <img src={Mockup} alt="Rajkosh Mockup" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SingleProject
+export default SingleProject;
